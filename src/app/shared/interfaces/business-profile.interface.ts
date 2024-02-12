c23a1320c08e286ea7599bfc4d9420f92f0b61ec
freedbrd <vadimtrihleb@gmail.com>
2024-02-12 22:13:26 +0100

    Update business-profile.interface.ts

--- src/app/shared/interfaces/business-profile.interface.ts
+++ src/app/shared/interfaces/business-profile.interface.ts
@@ -1,9 +1,11 @@
 export interface IBusinessProfile {
   id: string;
   name: string;
-  type: string;
+  type: BusinessProfileType;
   active: boolean;
+  currency: string;
   services: IProfileService[];
+  serviceResources: IServiceResource[]
 }
 
 export interface IProfileService {
@@ -16,10 +18,34 @@ export interface IProfileService {
   active: boolean;
   business_profile_id: string;
   user_id: string;
+  serviceResourceIds: string[]
 }
 
 export interface IServicePortfolio {
   id: string;
   url: string;
   description: string;
+}
+
+export enum BusinessProfileType {
+  SERVICE = 'service',
+  PRODUCT = 'product'
+}
+
+export interface IServiceResource {
+  id: string;
+  name: string;
+  type: string;
+  availability: IResourceAvailability[];
+  description: string;
+  image_url: string;
+  role: string;
+}
+
+export interface IResourceAvailability {
+  id: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  status: string;
 }
