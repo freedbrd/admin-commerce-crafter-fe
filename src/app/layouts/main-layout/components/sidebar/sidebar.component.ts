1df3dd8a74f6f46c2e124da7d07a79517998735b
freedbrd <vadimtrihleb@gmail.com>
2024-03-06 11:18:51 +0100

    added sidebar menu

--- src/app/layouts/main-layout/components/sidebar/sidebar.component.ts
+++ src/app/layouts/main-layout/components/sidebar/sidebar.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { ZorroModule } from '../../../../shared/modules/zorro/zorro.module';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    ZorroModule,
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
